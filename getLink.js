8aa5a1e7ba6918c8235ffb3be81e5da03445ca59
Wai Pai Lee <pailee.wai@gmail.com>
2018-01-03 23:23:54 +0800

    batch download

--- getLink.js
+++ getLink.js
@@ -6,7 +6,9 @@ function blockRequest(details) {
 function updateFilters(urls) {
    if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
      chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
+   
    chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: ["*://d.pcs.baidu.com/file*"]}, ['blocking']);
+   chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: ["*://www.baidupcs.com/rest/2.0/pcs/file*"]}, ['blocking']);
 }
 
 updateFilters();
